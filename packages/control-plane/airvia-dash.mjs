6fa7ecb251a52e434ad5bbee4b89942435970112
mohameddsh3ban <60802643+mohameddsh3ban@users.noreply.github.com>
2026-08-24 09:51:52 +0300

    chore: drop a scratch dashboard script committed by mistake

--- packages/control-plane/airvia-dash.mjs
+++
@@ -1,6 +0,0 @@
-import { openStore } from '@spicyspec/store';
-import { startControlPlane } from '@spicyspec/control-plane';
-const store = openStore('C:/XIII/share/Work/airvia/.spicyspec/runner.db');
-const cp = await startControlPlane({ store, projectName: 'Airvia', port: 4478 });
-console.log('up http://127.0.0.1:' + cp.port);
-await new Promise(() => {});
